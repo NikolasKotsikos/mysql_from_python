11c093946c4dbeb26ac9efb4578eb1bc054fa460
NikolasKotsikos <kotsikn@gmail.com>
2020-12-04 17:29:47 +0000

    alternate delete example in mysql-from-python.py

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import datetime
 import pymysql
 
 username = os.getenv('C9_USER')
@@ -10,9 +11,10 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-         sql = "SELECT * FROM Artist"
-         cursor.execute(sql)
-         result = cursor.fetchall()
-         print(result)
+         list_of_names = ['fred', 'Fred']
+         #Prepare a list with same number of placeholders as in list_of_names
+         format_strings = ','.join(['%s']*len(list_of_names))
+         cursor.execute("DELETE FROM Friends WHERE name in ({})".format(format_strings), list_of_names)
+         connection.commit()     
 finally:
     connection.close()
